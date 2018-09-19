46320d0fcb82c10d19e6e58b34534aef46d19995
Hugh Xie <hugh.xie@shopify.com>
2018-09-19 10:16:35 -0400

    revert to original tests

--- packages/react-shortcuts/src/tests/ShortcutManager.test.tsx
+++ packages/react-shortcuts/src/tests/ShortcutManager.test.tsx
@@ -5,59 +5,262 @@ import {timer} from '@shopify/jest-dom-mocks';
 import Key, {ModifierKey} from '../keys';
 import Shortcut from '../Shortcut';
 import ShortcutProvider from '../ShortcutProvider';
+import ShortcutWithFocus from './ShortcutWithRef';
 
-describe('<Shortcut />', () => {
-  describe('allowdefault', () => {
-    it('works', () => {
+describe('ShortcutManager', () => {
+  beforeEach(() => {
+    timer.mock();
+  });
+
+  afterEach(() => {
+    timer.restore();
+  });
+
+  it('calls the matching shortcut immediately if there are no other similar shortcuts', () => {
+    const fooSpy = jest.fn();
+    const barSpy = jest.fn();
+
+    mount(
+      <ShortcutProvider>
+        <Shortcut key="foo" ordered={['f', 'o', 'o']} onMatch={fooSpy} />
+        <Shortcut key="bar" ordered={['b', 'a', 'r']} onMatch={barSpy} />
+      </ShortcutProvider>,
+    );
+
+    keydown('f');
+    keydown('o');
+    keydown('o');
+
+    expect(fooSpy).toHaveBeenCalled();
+  });
+
+  it('calls multiple shortcuts', () => {
+    const fooSpy = jest.fn();
+    const barSpy = jest.fn();
+    mount(
+      <ShortcutProvider>
+        <Shortcut key="foo" ordered={['f', 'o', 'o']} onMatch={fooSpy} />
+        <Shortcut key="bar" ordered={['b', 'a', 'r']} onMatch={barSpy} />
+      </ShortcutProvider>,
+    );
+
+    keydown('f');
+    keydown('o');
+    keydown('o');
+
+    keydown('b');
+    keydown('a');
+    keydown('r');
+
+    expect(fooSpy).toHaveBeenCalledTimes(1);
+    expect(barSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('matches the longest fully matched shortcut when there are conflicting shortcuts after a timeout', () => {
+    const fooSpy = jest.fn();
+    const foSpy = jest.fn();
+    const fSpy = jest.fn();
+
+    mount(
+      <ShortcutProvider>
+        <Shortcut key="foo" ordered={['f', 'o', 'o']} onMatch={fooSpy} />
+        <Shortcut key="fo" ordered={['f', 'o']} onMatch={foSpy} />
+        <Shortcut key="f" ordered={['f']} onMatch={fSpy} />
+      </ShortcutProvider>,
+    );
+
+    keydown('f');
+    keydown('o');
+
+    expect(foSpy).not.toBeCalled();
+
+    timer.runAllTimers();
+
+    expect(fSpy).not.toHaveBeenCalled();
+    expect(foSpy).toHaveBeenCalledTimes(1);
+    expect(fooSpy).not.toHaveBeenCalled();
+  });
+
+  it('does not call shortcuts that do not match the keys pressed', () => {
+    const spy = jest.fn();
+    mount(
+      <ShortcutProvider>
+        <Shortcut ordered={['b', 'a', 'r']} onMatch={spy} />
+      </ShortcutProvider>,
+    );
+
+    keydown('b');
+    keydown('a');
+    keydown('z');
+
+    expect(spy).not.toBeCalled();
+
+    timer.runAllTimers();
+
+    expect(spy).not.toBeCalled();
+  });
+
+  it('does not call shortcuts that only partially match', () => {
+    const spy = jest.fn();
+    mount(
+      <ShortcutProvider>
+        <Shortcut key="foo" ordered={['f', 'o', 'o']} onMatch={spy} />
+        <Shortcut key="f" ordered={['f']} onMatch={spy} />
+      </ShortcutProvider>,
+    );
+
+    keydown('f');
+    keydown('o');
+    timer.runAllTimers();
+
+    expect(spy).not.toBeCalled();
+  });
+
+  it('calls shortcuts that are scoped to a specific node only when that node is focused', () => {
+    const spy = jest.fn();
+
+    const app = mount(
+      <ShortcutProvider>
+        <ShortcutWithFocus spy={spy} />
+      </ShortcutProvider>,
+    );
+    app.update();
+
+    keydown('z');
+    expect(spy).toBeCalled();
+  });
+
+  it('only registers a unique shortcut once', () => {
+    const spy = jest.fn();
+
+    mount(
+      <ShortcutProvider>
+        <Shortcut key="foo-1" ordered={['f', 'o', 'o']} onMatch={spy} />
+        <Shortcut key="foo-2" ordered={['f', 'o', 'o']} onMatch={spy} />
+      </ShortcutProvider>,
+    );
+
+    keydown('f');
+    keydown('o');
+    keydown('o');
+    timer.runAllTimers();
+
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+
+  it('unsubscribes keys when Shortcut unmounts', () => {
+    const spy = jest.fn();
+
+    const app = mount(
+      <ShortcutProvider>
+        <Shortcut key="bar" ordered={['b', 'a', 'r']} onMatch={spy} />
+        <Shortcut key="foo" ordered={['f', 'o', 'o']} onMatch={spy} />
+      </ShortcutProvider>,
+    );
+
+    app.unmount();
+
+    keydown('f');
+    keydown('o');
+    keydown('o');
+
+    keydown('b');
+    keydown('a');
+    keydown('r');
+
+    expect(spy).not.toBeCalled();
+  });
+
+  it('resets keys when there are no matching shortcuts', () => {
+    const spy = jest.fn();
+
+    mount(
+      <ShortcutProvider>
+        <Shortcut ordered={['?']} onMatch={spy} />
+      </ShortcutProvider>,
+    );
+
+    keydown('Shift');
+    keydown('a');
+    keydown('?');
+
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+
+  it('allows default event to occur', () => {
+    const spy = jest.fn();
+    const event = {
+      preventDefault: jest.fn(),
+    };
+
+    mount(
+      <ShortcutProvider>
+        <Shortcut ordered={['a']} onMatch={spy} allowDefault />
+      </ShortcutProvider>,
+    );
+
+    keydown('a', document, event);
+
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(event.preventDefault).not.toBeCalled();
+  });
+
+  it('prevents the default event by default', () => {
+    const spy = jest.fn();
+    const event = {
+      preventDefault: jest.fn(),
+    };
+
+    mount(
+      <ShortcutProvider>
+        <Shortcut ordered={['a']} onMatch={spy} />
+      </ShortcutProvider>,
+    );
+
+    keydown('a', document, event);
+
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(event.preventDefault).toBeCalled();
+  });
+
+  describe('modifier keys', () => {
+    it('matches shortcut when all modifier keys are pressed', () => {
       const fooSpy = jest.fn();
+      const held: ModifierKey[] = ['Control', 'Shift', 'Alt', 'Meta'];
 
       mount(
         <ShortcutProvider>
-          <Shortcut key="foo" ordered={['f', 'o', 'o']} onMatch={fooSpy} />
+          <Shortcut held={held} ordered={['/']} onMatch={fooSpy} />
         </ShortcutProvider>,
       );
 
-      keydown('f');
-      keydown('o');
-      keydown('o');
+      keydown('/', document, {
+        getModifierState: key => held.includes(key),
+      });
 
       expect(fooSpy).toHaveBeenCalled();
     });
+
+    it('doesn’t match shortcut when all modifier keys not pressed', () => {
+      const fooSpy = jest.fn();
+      const heldToCheck: ModifierKey[] = ['Control', 'Shift', 'Alt', 'Meta'];
+
+      mount(
+        <ShortcutProvider>
+          <Shortcut held={heldToCheck} ordered={['/']} onMatch={fooSpy} />
+        </ShortcutProvider>,
+      );
+
+      const heldPressed: ModifierKey[] = ['Control', 'Shift', 'Hyper'];
+
+      keydown('/', document, {
+        getModifierState: key => heldPressed.includes(key),
+      });
+
+      expect(fooSpy).not.toHaveBeenCalled();
+    });
   });
 });
-
-// describe('<Editor />', () => {
-//   describe('id', () => {
-//     it('uses a custom value', () => {
-//       const id = 'MyEditor';
-//       const editor = mount(<Editor id={id} />);
-//       expect(editor.find('textarea').prop('id')).toBe(id);
-//     });
-
-//     it('uses a default ID when not provided', () => {
-//       const editor = mount(<Editor />);
-//       expect(typeof editor.find('textarea').prop('id')).toBe('string');
-//     });
-//   });
-// });
-
-// it('calls the matching shortcut immediately if there are no other similar shortcuts', () => {
-//   const fooSpy = jest.fn();
-//   const barSpy = jest.fn();
-
-//   mount(
-//     <ShortcutProvider>
-//       <Shortcut key="foo" ordered={['f', 'o', 'o']} onMatch={fooSpy} />
-//       <Shortcut key="bar" ordered={['b', 'a', 'r']} onMatch={barSpy} />
-//     </ShortcutProvider>,
-//   );
-
-//   keydown('f');
-//   keydown('o');
-//   keydown('o');
-
-//   expect(fooSpy).toHaveBeenCalled();
-// });
 
 function keydown(key: Key | ModifierKey, target = document, eventSpies = {}) {
   let event = new KeyboardEvent('keydown', {
